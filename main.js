11082fb790a507e444c26741357095871b68409a
valeria <valeriairaismeneses@gmail.com>
2017-10-16 09:40:50 -0500

    Completado

---
+++ main.js
@@ -0,0 +1,14 @@
+var formulario = document.forms.login
+formulario.addEventListener('submit', function (e) {
+  e.preventDefault()
+  var name = formulario.elements[0].value
+  var lastName = formulario.elements[1].value
+  alert(name[0].toUpperCase() + " " + lastName[0].toUpperCase())
+  for (var i = 0; i < name.length; i++) {
+    console.log(name[0].toUpperCase());
+  }
+  for (var i = 0; i < lastName.length; i++) {
+    console.log(lastName[0].toUpperCase());
+  }
+
+})
